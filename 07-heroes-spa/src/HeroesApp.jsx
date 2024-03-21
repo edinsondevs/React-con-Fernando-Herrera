f0f220cb0932a253c3cb90b14bdd77e979527564
Edinson <94578362+edinsondevs@users.noreply.github.com>
2024-03-20 23:22:42 -0300

    agregando configuracion del useContext

--- 07-heroes-spa/src/HeroesApp.jsx
+++ 07-heroes-spa/src/HeroesApp.jsx
@@ -1,11 +1,12 @@
 
 import React from 'react'
 import { AppRouter } from './routes/AppRouter'
+import { AuthProvider } from './auth'
 
 export const HeroesApp = () => {
   return (
-    <>
+    <AuthProvider>
         <AppRouter />
-    </>
+    </AuthProvider>
   )
 }
---
+++ 07-heroes-spa/src/auth/types/types.js
@@ -0,0 +1,8 @@
+/* 
+*   Contiene el tipo de acciones centralizadas de nuestra aplicacion
+*/
+
+export const types = {
+    login:  '[Auth] Login',
+    logout: '[Auth] Logout',
+}
