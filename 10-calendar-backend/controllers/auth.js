71d51a465911a26eeaa0ecb54070b293f9aec9ba
Edinson <94578362+edinsondevs@users.noreply.github.com>
2024-08-29 00:06:19 -0300

    correccion de jwt

--- 10-calendar-backend/controllers/auth.js
+++ 10-calendar-backend/controllers/auth.js
@@ -1,31 +1,45 @@
 const { response } = require('express');
+const bcrypt = require('bcryptjs');
 const { validationResult } = require('express-validator');
 const Usuario = require('../models/Usuario');
+const { generarJWT } = require('../helpers/jwt');
 
-const createUser = async (req, res= response) => {
+const createUser = async (req, res = response) =>
+{
     const { correo } = req.body;
-    
-    try {        
+
+    try
+    {
         let usuario = await Usuario.findOne({ correo });
-        
-        if (usuario) {
+
+        if (usuario)
+        {
             return res.status(400).json({
                 ok: false,
                 message: 'El correo ya existe'
             })
         }
-        
-        usuario = new Usuario(req.body); 
+
+        usuario = new Usuario(req.body);
+        // ENCRIPTAR CONTRASEÑA
+        const salt = bcrypt.genSaltSync();
+        usuario.contrasena = bcrypt.hashSync(usuario.contrasena, salt);
+
         await usuario.save();
+
+        // GENERAR JWT
+        const token = await generarJWT(usuario._id, usuario.nombre);
 
         res.status(201).json({
             mensagge: 'Usuario Creado Correctamente',
             uid: usuario._id,
             nombre: usuario.nombre,
-            ok: false,
-        })        
-        
-    } catch (error) {
+            token,
+            ok: true,
+        })
+
+    } catch (error)
+    {
         console.log('error al crear usuario', error);
         res.status(500).json({
             ok: false,
@@ -33,34 +47,66 @@ const createUser = async (req, res= response) => {
         })
     }
 
-    
+
 };
 
-const renewToken = (req, res = response) => {
+const loginUser = async (req, res = response) =>
+{
+    const { correo, contrasena } = req.body;
+    const errors = validationResult(req);
+
+    try
+    {
+        let usuario = await Usuario.findOne({ correo });
+
+        if (!usuario)
+        {
+            return res.status(400).json({
+                ok: false,
+                message: 'Usuario y/o contraseña es incorrecto'
+            })
+        }
+
+        // CONFIRMAR LAS CONTRASEÑAS
+        const validPassword = bcrypt.compareSync(contrasena, usuario.contrasena);
+        if (!validPassword)
+        {
+            return res.status(400).json({
+                ok: false,
+                message: 'Usuario y/o contraseña es incorrecto'
+            })
+        }
+
+        // MANEJO DE AUTENTICACION - GENERANDO JSON WEB TOKEN
+        const token = await generarJWT(usuario._id, usuario.nombre);
+
+        // RESPONDER SI TODO ESTA OK
+        res.status(201).json({
+            mensagge: 'Usuario Logeado Correctamente',
+            uid: usuario._id,
+            nombre: usuario.nombre,
+            token,
+            ok: true,
+        })
+    } catch (error)
+    {
+        console.log('error al crear usuario', error);
+        res.status(500).json({
+            ok: false,
+            message: 'Error en el servidor'
+        })
+    }
+};
+
+
+const renewToken = (req, res = response) =>
+{
     res.json({
         ok: true,
         message: 'renew'
     })
 };
 
-const loginUser = (req, res= response) => {
-    const { usuario, edad, direccion, telefono, contrasena  } = req.body;
-    const errors = validationResult(req);
-
-    
-
-    res.json({
-        ok: true,
-        message: 'login',
-        user: {
-            usuario,
-            edad,
-            direccion,
-            telefono,
-            contrasena
-        }
-    })
-};
 
 module.exports = {
     createUser,
