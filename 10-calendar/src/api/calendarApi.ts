c0dd3a858a61b4ac92a9530d21160234b07077cb
Edinson <94578362+edinsondevs@users.noreply.github.com>
2024-09-07 14:59:19 -0300

    corrigiendo url prd

--- 10-calendar/src/api/calendarApi.ts
+++ 10-calendar/src/api/calendarApi.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 import { getEnvVariables } from "../helpers";
 
-const { VITE_API_URL, DEV, VITE_APR_URL_PRD } = getEnvVariables()
+const { VITE_API_URL, DEV, VITE_API_URL_PRD } = getEnvVariables();
 
 const calendarApi = axios.create({
-	baseURL: DEV ? VITE_API_URL : VITE_APR_URL_PRD,
+	baseURL: DEV ? VITE_API_URL : VITE_API_URL_PRD,
 });
 
 // todo: configurar interceptors
